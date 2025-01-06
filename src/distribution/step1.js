637a662d5096248dca4dc1890759e5994e5c23a3
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-01-06 08:43:47 +0100

    update code snippet

--- src/distribution/step1.js
+++ src/distribution/step1.js
@@ -19,23 +19,28 @@ import {step2Page} from "./step2.js";
   display.setAttribute("class","display");
 
   formDiv.innerHTML=`
+  
+  <div class="container">
     <h1 id="title"><img src="${orange}" alt="happy orange icon">OrangeTodo</h1>
-    <h1>Create your Profile</h1>
+    <h3>Create your Profile</h3>
    <form action="" method="post">
         <input type="E-mail" id="e-mail" placeholder="name@example.com" name="e-mail"></input>
-        <input type="text" id="first-name" placeholder="Your Name /n E.g Jane zest" name="first-name"></input>
+        <input type="text" id="first-name" placeholder="Your Name e.g Jane zest" name="first-name"></input>
         <p id="word-count">0/255</p>
-        <input type="radio" id="question" placeholder="I want to use OrangeTodo with my Team" name="question"></input>
+        <input type="radio" id="question" placeholder="I want to use OrangeTodo with my Team" name="question"></input>;
         <button id="continue-btn" type= "submit" >Continue</button>
 
 
    </form>
-  
+  </div>
    `
 
    display.innerHTML=`
+   <div class="container">
    <div id="page-count">Step 1 of 2</div>
    <img <img src="${backdrop}" alt="">
+   </div>
+
    `
 
 
