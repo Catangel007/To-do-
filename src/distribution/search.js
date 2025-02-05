ced0305c6d82f3f457e00f0355828cb93d923817
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-02-05 14:19:16 +0100

    build search box div

--- src/distribution/search.js
+++ src/distribution/search.js
@@ -6,19 +6,37 @@ import events from "../images/calendar_add.svg";
 import view from "../images/tune.svg";
 
 
-export  function searchPage () {
-
-    const content = document.querySelector("body");
 
 
-    const searchBox = document.querySelector("#container");
+    export  function searchPage(){
+      const searchBtn = document.querySelector("#search-btn");
+      
+      searchBtn.addEventListener("click",()=>{
+         const searchBox = document.createElement("div");
+         
+         searchBox.setAttribute("class","search-box");
+         searchBox.innerHTML = `
+         
+           <p><img src=""${search} alt="search icon"><input id="search-inbox" name ="search-inbox" type="text" placeholder="Search and type navigations"></p><hr>
+            <p>Recently viewed</p>
+             <p><img src=""${search} alt="search icon"> Today</p><hr>
+             <p>Navigation</p>
+            <p> <img src=""${search} alt="search icon"> Go to home</p>
+            <p> <img src=""${search} alt="search icon"> Go to Inbox</p>
+            <p> <img src=""${search} alt="search icon"> Go to Today</p>
+            <p> <img src=""${search} alt="search icon"> Go to Upcoming</p>
+            <p> <img src=""${search} alt="search icon"> Go to Filters and Labels</p>
+            <p> <img src=""${search} alt="search icon"> Go to Completed/p>
+            <p> <img src=""${search} alt="search icon"> Go to activity</p>
+             
+                 `;
+         content.appendChild(searchBox);
+        
+         searchBox.addEventListener("mouseleave", (e)=>{
+            content.removeChild(searchBox)
+          })
+      })
+    }
 
-   
-    content.innerHTML =`
-    <dialog open class="modal"> 
-             <input type="text" name="search" id="search" img="${search}"placeholder="Search Items">  
-      </dialog>
-      `
-}
 
 
