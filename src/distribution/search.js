106fcd3c3b6fadff3c64dcfe00d2d3d6086a6fb3
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-02-05 14:29:46 +0100

    Add: search box icons

--- src/distribution/search.js
+++ src/distribution/search.js
@@ -5,8 +5,13 @@ import close from "../images/close.svg";
 import events from "../images/calendar_add.svg";
 import view from "../images/tune.svg";
 
-
-
+import home from "src/images/home.svg";
+import inbox from "../images/inbox.svg";
+import today from "../images/calendar_today.svg";
+import upcoming from "../images/calendar_month.svg";
+import filters from "../images/grid3.svg";
+import complete from "../images/check_circle.svg";
+import activity from "../images/tune.svg";
 
     export  function searchPage(){
       const searchBtn = document.querySelector("#search-btn");
@@ -19,15 +24,15 @@ import view from "../images/tune.svg";
          
            <p><img src=""${search} alt="search icon"><input id="search-inbox" name ="search-inbox" type="text" placeholder="Search and type navigations"></p><hr>
             <p>Recently viewed</p>
-             <p><img src=""${search} alt="search icon"> Today</p><hr>
+             <p><img src=""${today} alt="search icon"> Today</p><hr>
              <p>Navigation</p>
-            <p> <img src=""${search} alt="search icon"> Go to home</p>
-            <p> <img src=""${search} alt="search icon"> Go to Inbox</p>
-            <p> <img src=""${search} alt="search icon"> Go to Today</p>
-            <p> <img src=""${search} alt="search icon"> Go to Upcoming</p>
-            <p> <img src=""${search} alt="search icon"> Go to Filters and Labels</p>
-            <p> <img src=""${search} alt="search icon"> Go to Completed/p>
-            <p> <img src=""${search} alt="search icon"> Go to activity</p>
+            <p> <img src=""${home} alt="search icon"> Go to home</p>
+            <p> <img src=""${inbox} alt="search icon"> Go to Inbox</p>
+            <p> <img src=""${today} alt="search icon"> Go to Today</p>
+            <p> <img src=""${upcoming} alt="search icon"> Go to Upcoming</p>
+            <p> <img src=""${filters} alt="search icon"> Go to Filters and Labels</p>
+            <p> <img src=""${complete} alt="search icon"> Go to Completed/p>
+            <p> <img src=""${activity} alt="search icon"> Go to activity</p>
              
                  `;
          content.appendChild(searchBox);
