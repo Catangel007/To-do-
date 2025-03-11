222b26ce2c4b1c556071b364704bbb9926ce7792
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-03-11 07:28:19 +0100

    Update home.js

--- src/distribution/home.js
+++ src/distribution/home.js
@@ -50,7 +50,7 @@ import playingCards from "../images/playing_cards.svg";
    import { teamPage }  from "./team.js";
    import { templatePage }  from "./template.js";
    import { calendarPage } from "./calendar.js";
-   
+   import { todoManager } from "./todo-manager.js";
  
  export function homePage(){
    
@@ -148,128 +148,9 @@ function displayPage(list) {
 }
 
 
-// manage new todo and old ones
-class TodoManager {
-
-  constructor() {
-      this.todos = [];
-      this.projects = ["Home","Today","Tomorrow","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Custom"];
-      this.filters = {
-          priority: null,
-          dueDate: null,
-          completed: null
-      };
-  }
-
-  addTodo(todo) {
-      this.todos.push(todo);
-      this.saveTodos();
-      this.renderTodos();
-  }
-
-  deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-      this.saveTodos();
-      this.renderTodos();
-  }
-
-  toggleTodoComplete(id) {
-      const todo = this.todos.find(t => t.id === id);
-      if (todo) {
-          todo.completed = !todo.completed;
-          this.saveTodos();
-          this.renderTodos();
-      }
-  }
-
-  saveTodos() {
-      localStorage.setItem('todos', JSON.stringify(this.todos));
-  }
-
-  loadTodos() {
-      const saved = localStorage.getItem('todos');
-      this.todos = saved ? JSON.parse(saved) : [];
-      this.renderTodos();
-  }
-
-  renderTodos() {
-      const container = document.getElementById('newTodo');
-      container.innerHTML = '';
-
-      const filteredTodos = this.getFilteredTodos();
-      
-      filteredTodos.forEach(todo => {
-          const todoElement = this.createTodoElement(todo);
-          container.appendChild(todoElement);
-      });
-
-      this.updateTaskCounter();
-  }
-
-  getFilteredTodos() {
-      return this.todos.filter(todo => {
-          if (this.filters.priority && todo.priority !== this.filters.priority) return false;
-          if (this.filters.completed !== null && todo.completed !== this.filters.completed) return false;
-          if (this.filters.dueDate) {
-              const today = new Date().toISOString().split('T')[0];
-              if (this.filters.dueDate === 'today' && todo.dueDate !== today) return false;
-
-          }
-          return true;
-      });
-  }
-
-  updateTodo(id,updateData){
-    this.todos = this.todos.map(todo => {
-      if (todo.id === id){
-        return{...todo,...updateData};
-      }
-      return todo;
-    });
-    this.saveTodos();
-    this.renderTodos();
-  }
-
-   createTodoElement(todo) {
-     const div = document.createElement("div");
-     div.className = "task";
-     div.id = `todo-${todo.id}`;
-    
-     
-     div.innerHTML = `
-             <img src="${grig2}">
-             <input type="checkbox" ${todo.completed ? 'checked' : ''} id="radio-btn" name="radio-btn" >
-             <span class="todo-title">${todo.title}</span>
-            ${todo.description ? `<p class="todo-description">${todo.description}</p>`: ''}
-            <div class="todo-footer">
-            <span class="priority-badge priority-${todo.priority.split(" ")[1]}">${todo.priority}</span>
-            <span class="due-date"${todo.dueDate}></span>
-              
-            </div>
-     `;
-   
-// Add event listeners
-const checkbox = div.querySelector('input[type="checkbox"]');
-checkbox.addEventListener('change', () => this.toggleTodoComplete(todo.id));
-
-   return div;  
-   }
-    
-
-
-  updateTaskCounter() {
-      const counter = document.getElementById('taskCounter');
-      const completed = this.todos.filter(todo => todo.completed).length;
-      counter.innerHTML = `
-          <img src="${checkCircle}">
-          ${completed}/${this.todos.length} tasks completed
-      `;
-  }
-}
 
 
 // Initialize TodoManager
-const todoManager = new TodoManager();
 todoManager.loadTodos();
 
 // Add drag and drop functionality
